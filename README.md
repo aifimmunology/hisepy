06f3ac9d0aa25644c09af5a7425f94fb65046972
Paul Mariz <paul.mariz@alleninstitute.org>
2021-03-31 17:01:08 -0700

    First

--- README.md
+++
@@ -1,9 +0,0 @@
-# hise-python-sdk
-The Python package for HISE IDE Images.
-
-```
-import hisepy
-```
-
-Documentation TK
-
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import os
+import sys
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+if sys.version_info[0] < 3:
+    with open(os.path.join(_here, 'README.rst')) as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(_here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+
+version = {}
+with open(os.path.join(_here, 'hisepy', 'version.py')) as f:
+    exec(f.read(), version)
+
+setup(
+    name='hisepy',
+    version=version['__version__'],
+    description=('.'),
+    long_description=long_description,
+    author='Paul Mariz',
+    author_email='paul.mariz@alleninstitute.org',
+    url='https://github.com/aifimmunology/hisepy',
+    packages=['hisepy'],
+    install_requires=[
+        'nptsne',
+        'pyreadr',
+        'pandas',
+        'numpy',
+        'networkx',
+        'community.community_louvain'        
+    ],
+    #   scripts=['],
+    include_package_data=True,
+    classifiers=[
+        'Immunology',
+        'Statistical Regression',
+        'Programming Language :: Python :: 3.8'],
+)
