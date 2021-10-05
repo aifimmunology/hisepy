925eff286db21e91090f44ec6c1a33fe895aab05
james.harvey <james.harvey@alleninstitute.org>
2021-10-05 14:11:59 -0700

    adding new formatter module

--- hisepy/__init__.py
+++ hisepy/__init__.py
@@ -1,7 +1,7 @@
 from .version import __version__
 from .reader import hise_file, read_files, read_subjects, read_samples
 from .scheduler import notebook_job, schedule_notebook, get_notebook_job, clear_notebook_job
-
+from .formatter import descriptors_to_df
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
 __all__ = [
@@ -12,5 +12,6 @@ __all__ = [
     'schedule_notebook',
     'get_notebook_job',
     'clear_notebook_job',
-    'notebook_job'
+    'notebook_job',
+    'descriptors_to_df'
 ]
