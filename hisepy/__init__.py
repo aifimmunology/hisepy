31462301ca897ce4049cc5a4e36518d7d19d0d9d
james.harvey <james.harvey@alleninstitute.org>
2021-10-12 15:13:59 -0700

    first-pass project_folder methods

--- hisepy/__init__.py
+++ hisepy/__init__.py
@@ -1,8 +1,12 @@
 from .version import __version__
 from .reader import hise_file, read_files, read_subjects, read_samples
 from .scheduler import notebook_job, schedule_notebook, get_notebook_job, clear_notebook_job
-from .project_folder import list_files_in_project_folders
-
+from .project_folder import (
+    list_project_folders, 
+    list_files_in_project_folder, 
+    download_from_project_folder, 
+    archive_file_in_project_folder
+)
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
 __all__ = [
