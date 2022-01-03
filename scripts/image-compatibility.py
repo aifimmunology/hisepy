5ee63bb3147d4ed48da6423f54a2cb0ed2c4cf7b
Paul Mariz <paul.mariz@alleninstitute.org>
2022-01-03 15:04:34 -0800

    Test script

---
+++ scripts/image-compatibility.py
@@ -0,0 +1,26 @@
+import plotly.graph_objects as go
+import pandas as pd
+import hisepy
+import requests
+trace_id = "25d7f47a-183f-4894-83a7-28cd472b9dd0"
+
+layout = hisepy.reader.parse_hise_response(
+    requests.request("GET",
+                     hisepy.reader.hise_url("toolchain", "visualization_path", trace_id),
+                     headers = hisepy.auth.get_bearer_token_header()))
+layout["layout"].pop("margin")
+layout["layout"].pop("autocolorscale")
+fig = go.Figure(layout)
+tr = hisepy.get_trace(trace_id)
+for d in hisepy.load_visualization_data(tr["steps"]["dataReference"]):
+    d.pop("xField")
+    d.pop("yField")
+    d["marker"].pop("color")
+    d["marker"].pop("size")
+    fig.add_trace(d)
+
+fig.show()
+
+fixed = hisepy.load_visualization(trace_id)
+fixed.show()
+
