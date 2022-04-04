34c53de8cf6105026a667b854a8e0ce318b38b6c
Andrew Buchanan <andrew.buchanan@alleninstitute.org>
2022-04-04 16:45:07 -0700

    Autoformat and add pre-commit autoformatting
    Autoformats all source via [yapf](https://github.com/google/yapf).
    
    Also adds configuration for [pre-commit](https://pre-commit.com/) to run `yapf` on every commit to maintain the nice formatting.
    
    Also adds CONTRIBUTING.md with instructions for installing `pre-commit`.
    
    Also some other static analysis fixups:
    * Update setup.py with missing requirements (requests, h5py, google)
    * Drop unnecessary parentheses
    * Switch to triple double-quotes for docstrings because that's what PEP8 wants
    * Drop empty docstrings
    * Fix some docstring parameter list / actual parameter list mismatches
    * Drop unused imports
    * Drop leftover debugger breakpoint

--- scripts/dash-poc/server.py
+++ scripts/dash-poc/server.py
@@ -1,21 +1,20 @@
 import http.server
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
 
 PORT = 8080
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-Handler.extensions_map={
-        '.manifest': 'text/cache-manifest',
-        '.html': 'text/html',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.svg':'image/svg+xml',
-        '.css':'text/css',
-        '.js':'application/x-javascript',
-        '': 'application/json', # Default
-    }
+Handler.extensions_map = {
+    '.manifest': 'text/cache-manifest',
+    '.html': 'text/html',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg': 'image/svg+xml',
+    '.css': 'text/css',
+    '.js': 'application/x-javascript',
+    '': 'application/json',  # Default
+}
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
