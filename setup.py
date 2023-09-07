1a89080e9e8611c52b260f76beb08ac5250b7cd3
jamesdharv <james.harvey@alleninstitute.org>
2023-09-07 11:36:49 -0700

    removing dash, which is dependent on plotly

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=['hisepy'],
     install_requires=[
         'pandas', 'numpy', 'PyYAML', 'scipy', 'anndata',
-        'google-cloud-storage', 'importlib', 'dash', 'Frozen-Flask', 'kaleido',
+        'google-cloud-storage', 'importlib', 'Frozen-Flask', 'kaleido',
         'requests', 'h5py', 'google', 'pipreqs', 'pip-tools', 'pyreadr',
         'termcolor'
     ],
