67cd7c140bd0a6418646b2febfaae8bcbc2f8b2f
jamesdharv <james.harvey@alleninstitute.org>
2023-11-01 11:10:37 -0700

    removing scRNA_utils module and scipy + anndata from setup

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ setup(
     url='https://github.com/aifimmunology/hisepy',
     packages=['hisepy'],
     install_requires=[
-        'pandas', 'numpy', 'PyYAML', 'scipy', 'anndata', 'plotly', 'dash',
-        'kaleido', 'google-cloud-storage', 'importlib', 'Frozen-Flask',
-        'requests', 'h5py', 'google', 'pipreqs==0.4.12', 'pip-tools',
-        'pyreadr', 'termcolor'
+        'pandas', 'numpy', 'PyYAML', 'plotly', 'dash', 'kaleido',
+        'google-cloud-storage', 'importlib', 'Frozen-Flask', 'requests',
+        'h5py', 'google', 'pipreqs==0.4.12', 'pip-tools', 'pyreadr',
+        'termcolor'
     ],
     scripts=['hisepy/config.yaml'],
     include_package_data=True,
