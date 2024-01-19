b2e1baa91306bf188478e3f6818defb6aa036c46
madeline-ambrose-ai <madeline.ambrose@alleninstitute.org>
2024-01-19 10:17:29 -0800

    remove configFileId from save_abstraction return

--- hisepy/abstraction.py
+++ hisepy/abstraction.py
@@ -271,6 +271,5 @@ def save_abstraction(app_filepath: str = None,
 
         return {
             "message": resp["Message"],
-            "AbstractionId": resp["AbstractionId"],
-            "configFileId": resp["ConfigFileId"]
+            "AbstractionId": resp["AbstractionId"]
         }
