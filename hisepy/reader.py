f77cbf20a2b7d7ceec2faf4f48552d2a1e3c96db
jamesdharv <james.harvey@alleninstitute.org>
2024-04-01 16:51:11 -0700

    download in chunks

--- hisepy/reader.py
+++ hisepy/reader.py
@@ -476,7 +476,10 @@ def cache_file(url: str, file_name: str, file_dir: str):
                 "Request to get file %s failed with status %d. %s" %
                 (file_name, resp.status_code, resp.text))
         else:
-            open(f_path, 'wb').write(resp.content)
+            with open(f_path, 'wb') as file:
+                for chunk in resp.iter_content(
+                        chunk_size=CONFIG['IDE']["DOWNLOAD_CHUNK_SIZE"]):
+                    file.write(chunk)
 
 
 def read_samples(sample_ids=None, query_dict=None, to_df=True):
