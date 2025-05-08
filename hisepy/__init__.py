400604530e02c4af81347ca182c56eadf8298f2c
jamesdharv <james.harvey@alleninstitute.org>
2025-05-08 12:40:31 -0700

    list training job methods

--- hisepy/__init__.py
+++ hisepy/__init__.py
@@ -30,6 +30,7 @@ from .private_folders import (
     upload_file_to_private_folder, delete_private_folder,
     find_private_folder_of_file)
 from .instances import (stop_ide)
+from .training_job import (list_all_jobs, get_job) 
 from .version import __version__
 
 # if somebody does "from somepackage import *", this is what they will
