2fffaeef2b67689c7dda65ed32c2d580234c4d59
jamesdharv <james.harvey@alleninstitute.org>
2025-07-02 12:22:05 -0700

    remove hero_image_path param

--- hisepy/conda_pack.py
+++ hisepy/conda_pack.py
@@ -12,9 +12,7 @@ CONFIG = cu.read_yaml('{}/config.yaml'.format(_here))
 def validate_conda_env_params(
     env_name : str,
     description: str,
-    hero_image_path : str,
-    languages: list[str],
-    tags : dict
+    languages: list[str]
 ):
     """
     Validate parameters for saving a custom conda environment.
@@ -22,9 +20,7 @@ def validate_conda_env_params(
     Args:
         env_name (str): Name of the conda environment.
         description (str): Description of the environment.
-        hero_image_path (str): Path to the hero image for the environment. This image will show in the HISE UI.
         languages (list[str]): List of programming languages supported by the environment.
-        tags (list[str]): List of tags associated with the environment.
 
     Raises:
         ValueError: If any parameter is invalid.
@@ -33,10 +29,6 @@ def validate_conda_env_params(
         raise ValueError("env_name must be a non-empty string.")
     if not isinstance(description, str):
         raise ValueError("description must be a string.")
-    if cu.string_contains_whitespaces(hero_image_path):
-        raise ValueError("hero_image_path must not contain whitespace characters.")
-    if not isinstance(hero_image_path, str) or not hero_image_path.endswith(('.png')):
-        raise ValueError("hero_image_path must be of type 'png'")
     if not isinstance(languages, list) or not all(isinstance(lang, str) for lang in languages):
         raise ValueError("languages must be a list of strings.")
     
@@ -45,16 +37,12 @@ def validate_conda_env_params(
     if not all(lang.capitalize() in valid_languages for lang in languages):
         raise ValueError(f"languages must be one of {valid_languages}.")
 
-    if not isinstance(tags, dict) and tags is not None:
-        raise ValueError("tags must be a dictionary or None if not provided.")
     return 
 
 def save_custom_conda_environment(
     env_name : str,
     description: str,
-    hero_image_path : str, 
-    languages: list[str], 
-    tags : dict = None
+    languages: list[str]
 ):
     """
     Save a custom conda environment with additional metadata.
@@ -62,23 +50,19 @@ def save_custom_conda_environment(
     Parameters:
         env_name (str): Name of the conda environment. This is the name that will show in the HISE UI.
         description (str): Description of the environment.
-        hero_image_path (str): Path to the hero image for the environment.
         languages (list[str]): List of programming languages supported by the environment.
-        tags (list[str]): List of tags associated with the environment.
     Returns: 
         dict: Response from the HISE API after saving the environment.
     Example: 
         >>> save_custom_conda_environment(
                 env_name="my_custom_env",
                 description="A custom conda environment for data science.",
-                hero_image_path="/path/to/hero_image.png",
                 languages=["python", "r"],
-                tags=["data science", "custom"]
             )
     """
 
     # Validate parameters
-    validate_conda_env_params(env_name, description, hero_image_path, languages, tags)
+    validate_conda_env_params(env_name, description, languages)
 
     # prompt user that active environment will be saved
     cu.prompt_user(CONFIG["PROMPTS"]["SAVE_CUSTOM_CONDA_ENV"])
@@ -106,9 +90,8 @@ def save_custom_conda_environment(
     params = {
         "name": env_name,
         "description": description,
-        "heroImagePath": hero_image_path,
         "language": languages,
-        "ownerEmail" : HiseUser().email
+        "ownerEmail" : HiseUser().email,
     }
     with open(yaml_path, 'rb') as f:
         files = {
