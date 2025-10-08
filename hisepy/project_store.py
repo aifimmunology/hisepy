938a0ea60ece381f98648c4c515eccea5fe35e5e
jamesdharv <james.harvey@alleninstitute.org>
2025-10-08 11:08:15 -0700

    refactor project store methods for better error handling

--- hisepy/project_store.py
+++ hisepy/project_store.py
@@ -13,13 +13,15 @@ import requests
 
 import hisepy.common_utils as cu
 from hisepy.auth import get_bearer_token_header, hise_server, IDEInstance, guest_hise_server, ide_is_from_guest_account
+from hisepy.logging import with_default_logging, logger
 
 # load config for global variables and endpoints
 _here = os.path.abspath(os.path.dirname(__file__))
 CONFIG = cu.read_yaml('{}/config.yaml'.format(_here))
 
 
-def list_project_stores():
+@with_default_logging
+def list_project_stores() -> list:
     """
     Lists all project stores a user has access to
 
@@ -43,7 +45,8 @@ def list_project_stores():
     return project_list
 
 
-def list_files_in_project_store(store_name):
+@with_default_logging
+def list_files_in_project_store(store_name: str):
     """
     Returns information about what files are present in a given project store
 
@@ -52,6 +55,9 @@ def list_files_in_project_store(store_name):
     Returns:
         data.frame containing fileIds and fileNames
     """
+    if type(store_name) is not str:
+        raise ValueError("store_name must be of type str")
+
     store = {'stores': [store_name]}
     url = 'https://{ser}/{hy}/{pfe}/{f}'.format(
         ser=hise_server(),
@@ -76,7 +82,10 @@ def list_files_in_project_store(store_name):
     return df
 
 
-def download_from_project_store_v1(store_name, file_name='', subdir=''):
+@with_default_logging
+def download_from_project_store_v1(store_name: str,
+                                   file_name: str = None,
+                                   subdir: str = None):
     """
     Downloads a given file onto a user's IDE. The filepath pattern is as follows:
     '~/store_name/file_name'.
@@ -88,6 +97,8 @@ def download_from_project_store_v1(store_name, file_name='', subdir=''):
     Returns:
         True if download was successful
     """
+    if type(store_name) is not str:
+        raise ValueError("store_name must be of type str")
 
     def _submit_url_download(url: str, store: str, filen: str):
         if '/' not in filen:
@@ -140,8 +151,9 @@ def download_from_project_store_v1(store_name, file_name='', subdir=''):
                 fn=i)
             _submit_url_download(this_url, store_name, i)
             ps_file_id = ps_df.loc[ps_df['name'].eq(i), 'id'].item()
-            
-            cu.log_downloaded_files(ps_file_id, None, CONFIG['STORES']['TEMP_STORE'])
+
+            cu.log_downloaded_files(ps_file_id, None,
+                                    CONFIG['STORES']['TEMP_STORE'])
     else:
         # create url download
         url = 'https://{ser}/{hy}/{pfe}/{fol}/{fil}/{fn}'.format(
@@ -153,73 +165,97 @@ def download_from_project_store_v1(store_name, file_name='', subdir=''):
             fn=file_name)
         _submit_url_download(url, store_name, file_name)
         ps_file_id = ps_df.loc[ps_df['name'].eq(file_name), 'id'].item()
-        cu.log_downloaded_files(ps_file_id, None, CONFIG['STORES']['TEMP_STORE'])
+        cu.log_downloaded_files(ps_file_id, None,
+                                CONFIG['STORES']['TEMP_STORE'])
     return True
 
 
-def download_from_project_store(store_name, file_name='', subdir=''):
+@with_default_logging
+def download_from_project_store(store_name: str,
+                                file_name: str = None,
+                                subdir: str = None) -> bool:
     """
     Downloads a given file onto a user's IDE. The filepath pattern is as follows:
     '~/store_name/file_name'.
 
     Parameters:
         store_name (str): name of project store
-        file_name (str): name of file that you see under 'name' when utilizing 
-            list_files_in_project_store
+        file_name (str, optional): Name of the specific file to download.
+        subdir (str, optional): Subdirectory name; if specified, downloads all
+            files under that subdirectory.
     Returns:
         True if download was successful
     """
 
-    ps_df = list_files_in_project_store(store_name)[['name', 'id']]
+    # validate input parameters
+    if not isinstance(store_name, str):
+        raise ValueError("`store_name` must be a string.")
+    if file_name is not None and not isinstance(file_name, str):
+        raise ValueError("`file_name` must be a string if provided.")
+    if subdir is not None and not isinstance(subdir, str):
+        raise ValueError("`subdir` must be a string if provided.")
+    if not store_name.strip():
+        raise ValueError("`store_name` cannot be empty.")
+    if not file_name and not subdir:
+        raise ValueError(
+            "Specify either `file_name` or `subdir` to download from.")
 
-    # case where user wants to download all files within a subdir they uploaded
+    # retrieve available files
+    try:
+        ps_df = list_files_in_project_store(store_name)[['name', 'id']]
+    except Exception as e:
+        raise RuntimeError(
+            f"Failed to list files in project store '{store_name}': {e}")
+
     ide_name = IDEInstance().podName
-    if (file_name == '') & (subdir != ''):
-        # find all files that has that subdir in name
-        list_files = list_files_in_project_store(
-            store_name)['name'].unique().tolist()
+    files_to_download = []
 
-        # subset to entries with '/<subdir>/' in name
-        subdir_files = [x for x in list_files if '/{}/'.format(subdir) in x]
-
-        # create urls for each file in subset
-        url_list = []
-        for i in subdir_files:
-            this_url = 'https://{ser}/{hy}/{pfe}/{fol}/{ide}/{fil}/{fn}'.format(
-                ser=hise_server(),
-                hy=CONFIG['HYDRATION']['HYDRATION_NAME'],
-                pfe=CONFIG['PROJECT_STORE']['PROJECT_STORE_ENDPOINT'],
-                fol=store_name,
-                ide=ide_name,
-                fil='files',
-                fn=i)
-            resp = cu.parse_hise_response(
-                requests.request("GET", url,
-                                 headers=get_bearer_token_header()))
-            ps_file_id = ps_df.loc[ps_df['name'].eq(i), 'id'].item()
-
-            cu.log_downloaded_files(ps_file_id, None, CONFIG['STORES']['TEMP_STORE'])
-            print("file {}, downloaded to {}".format(i, resp['file']))
+    # determine which files to download
+    if subdir:
+        all_files = ps_df['name'].unique().tolist()
+        files_to_download = [f for f in all_files if f'/{subdir}/' in f]
+        if not files_to_download:
+            raise ValueError(f"No files found in subdirectory '{subdir}'.")
     else:
-        # create url download
-        url = 'https://{ser}/{hy}/{pfe}/{fol}/{ide}/{fil}/{fn}'.format(
-            ser=hise_server(),
-            hy=CONFIG['HYDRATION']['HYDRATION_NAME'],
-            pfe=CONFIG['PROJECT_STORE']['PROJECT_STORE_ENDPOINT'],
-            fol=store_name,
-            ide=ide_name,
-            fil='files',
-            fn=file_name)
-        resp = cu.parse_hise_response(
-            requests.request("GET", url, headers=get_bearer_token_header()))
-        ps_file_id = ps_df.loc[ps_df['name'].eq(file_name), 'id'].item()
-        cu.log_downloaded_files(ps_file_id, None, CONFIG['STORES']['TEMP_STORE'])
-        print("file {}, successfully downloaded to {}".format(
-            file_name, resp['file']))
+        if file_name not in ps_df['name'].values:
+            raise ValueError(
+                f"File '{file_name}' not found in store '{store_name}'.")
+        files_to_download = [file_name]
+
+    # download each file
+    for f in files_to_download:
+        url = (f"https://{hise_server()}/"
+               f"{CONFIG['HYDRATION']['HYDRATION_NAME']}/"
+               f"{CONFIG['PROJECT_STORE']['PROJECT_STORE_ENDPOINT']}/"
+               f"{store_name}/{ide_name}/files/{f}")
+
+        try:
+            response = requests.get(url,
+                                    headers=get_bearer_token_header(),
+                                    timeout=30)
+            obj = cu.parse_hise_response(response)
+            local_path = obj.get("file")
+            ps_file_id = ps_df.loc[ps_df['name'].eq(f), 'id'].item()
+            cu.log_downloaded_files(ps_file_id, None,
+                                    CONFIG['STORES']['TEMP_STORE'])
+
+            logger.info(f"File '{f}' successfully downloaded to {local_path}")
+
+        except requests.Timeout:
+            raise RuntimeError(f"Download timed out for file '{f}'.")
+        except requests.RequestException as e:
+            raise RuntimeError(f"Network error while downloading '{f}': {e}")
+        except Exception as e:
+            raise RuntimeError(f"Failed to process file '{f}': {e}")
+
+    logger.info(
+        f"Successfully downloaded {len(files_to_download)} file(s) from store '{store_name}'."
+    )
     return True
 
 
-def promote_file_in_project_store(store_name, file_name):
+@with_default_logging
+def promote_file_in_project_store(store_name: str, file_name: str) -> bool:
     """
     Mark a file in a project store to be promoted to the permanent store. 
     Promoted files will not be listed in hp.list_files_in_project_store()
@@ -234,7 +270,8 @@ def promote_file_in_project_store(store_name, file_name):
                                      CONFIG['PROJECT_STORE']['PROMOTION_TAG'])
 
 
-def undo_promote_in_project_store(store_name, file_name):
+@with_default_logging
+def undo_promote_in_project_store(store_name: str, file_name: str) -> bool:
     """
     Undoes the promotion action, so long as the file 
     has not already been moved to the permanent store.
@@ -250,7 +287,8 @@ def undo_promote_in_project_store(store_name, file_name):
                                      CONFIG['PROJECT_STORE']['AVAILABLE_TAG'])
 
 
-def delete_file_in_project_store(store_name, file_name):
+@with_default_logging
+def delete_file_in_project_store(store_name: str, file_name: str) -> bool:
     """
     Deletes a file in the project store, so long as it is not otherwise in use
     The file will not be visible through list_files_in_project_store()    
@@ -265,7 +303,8 @@ def delete_file_in_project_store(store_name, file_name):
                                      CONFIG['PROJECT_STORE']['DELETED_TAG'])
 
 
-def undo_delete_in_project_store(store_name, file_name):
+@with_default_logging
+def undo_delete_in_project_store(store_name: str, file_name: str) -> bool:
     """
     Undoes the file delete action, so long as it is within the file's retention period
     (usually 90 days)
