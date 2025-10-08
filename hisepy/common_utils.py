f48f389881ea3b77248172b1f939d33c6e393079
jamesdharv <james.harvey@alleninstitute.org>
2025-10-08 15:18:27 -0700

    refactor private folder methods and implement logging framework

--- hisepy/common_utils.py
+++ hisepy/common_utils.py
@@ -143,34 +143,6 @@ def debug_config_value(heading: str, key: str):
     return os.getenv("HISEPY_%s_%s" % (heading.upper(), key.upper()))
 
 
-def download_response_content(resp, dest):
-    # check status
-    if resp.status_code != 200:
-        raise SystemError(
-            "%s request to %s returned with status %d. %s" %
-            (resp.request.method, resp.url, resp.status_code, resp.text))
-
-    # separate filename and path
-    dest_list = dest.split('/')
-    this_file_name = dest_list[-1]
-    dest_list.pop()
-    this_path = '/'.join(dest_list)
-    if '.' not in this_file_name:
-        raise SystemError("Unable to parse out fileName, %s" %
-                          (this_file_name))
-
-    # create directory if it doesn't exist; download
-    pathlib.Path(this_path).mkdir(parents=True, exist_ok=True)
-    if not os.path.isdir(this_path):
-        raise SystemError("unable to create path, %s" % (this_path))
-
-    with open(dest, 'wb') as f:
-        for chunk in resp.iter_content(CONFIG['IDE']['DOWNLOAD_CHUNK_SIZE']):
-            f.write(chunk)
-    print('file successfully downloaded: {}'.format(dest))
-    return
-
-
 def find_files(directory, filenames):
     """ Given a directory, find all files in a given list """
     files_list = []
