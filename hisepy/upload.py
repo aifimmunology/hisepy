9ab5be9f2f10790900fed49fcd055422d427b03f
jamesdharv <james.harvey@alleninstitute.org>
2025-11-21 08:54:31 -0800

    missing comma...

--- hisepy/upload.py
+++ hisepy/upload.py
@@ -694,7 +694,7 @@ def upload_files_fast_mode(files: list,
                         store=store, 
                         destination=destination,
                         do_conda_build_check=False,
-                        use_fast_mode=True
+                        use_fast_mode=True,
                         do_prompt=do_prompt)
     finally: 
         # always reset 
