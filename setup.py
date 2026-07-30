bfc39e8a1c982cddfb86dd5a8891bfc61bcd43ba
jamesdharv <james.harvey@alleninstitute.org>
2026-07-30 12:22:30 -0700

    troubleshooting

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 _here = os.path.abspath(os.path.dirname(__file__))
-
+about = {}
+with open(os.path.join(_here, 'hisepy', 'version.py')) as f:
+    exec(f.read(), about)
 if sys.version_info[0] < 3:
     with open(os.path.join(_here, 'README.rst')) as f:
         long_description = f.read()
@@ -24,7 +26,8 @@ setup(
     use_scm_version={
         "write_to": "hisepy/version.py",
     },
-    setup_requires=["setuptools_scm"],
+    #setup_requires=["setuptools_scm"],
+    version=about['__version__']
     scripts=['hisepy/config.yaml'],
     include_package_data=True,
     classifiers=[
